f4bdabb9eb6811944d22a0c175381fd33207fbef
JongukYang <didwhddnr123@naver.com>
2022-11-04 14:12:48 +0900

    Update Pro_CT_17.java

--- Pro_CT_17.java
+++ Pro_CT_17.java
@@ -17,7 +17,7 @@ public class Pro_CT_17 {
         System.out.println(sol.solution(p));
     }
 }
-// 오늘은 MOD 하기 때문에 2
+// 오늘까지 mod 합니당
 
 //for (int i = 0; i < phone_number.length() - 4; i++)
 //        answer += "*";
