7c91ed653e618f84ca5c5d5fa5729c6bd6860e46
JongukYang <didwhddnr123@naver.com>
2022-11-22 14:15:32 +0900

    Create Pro_CT_44.java

---
+++ Pro_CT_44.java
@@ -0,0 +1,13 @@
+public class Pro_CT_44 {
+//    폰켓몬
+    public int solution(int[] nums) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pro_CT_44 sol = new Pro_CT_44();
+        int[] nums = {3, 1, 2, 3}; // result = 2
+        System.out.println(sol.solution(nums));
+    }
+}
