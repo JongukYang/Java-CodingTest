1c5f5d235c61250a67b38aecbbbbd6abc9e55d46
JongukYang <didwhddnr123@naver.com>
2022-12-02 16:31:33 +0900

    50

---
+++ Pro_CT_49.java
@@ -0,0 +1,7 @@
+public class Pro_CT_49 {
+    // kakao코테 잠깐 미뤄두기
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Pro_CT_50.java
@@ -0,0 +1,17 @@
+public class Pro_CT_50 {
+    // 콜라 문제
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pro_CT_50 sol = new Pro_CT_50();
+        int a = 2; // 마트에서 콜라로 바꿔줄 때 제공해야하는 빈병의 개수
+        int b = 1; // 마트가 주는 빈병의 개수
+        int n = 20; // 초기 빈병
+        System.out.println(sol.solution(a,b,n));
+    }
+}
