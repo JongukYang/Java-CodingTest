62402d8f462c5e1c9d92f61c9097e9645ac08933
JongukYang <didwhddnr123@naver.com>
2022-12-08 16:06:06 +0900

    53

--- Pro_CT_34.java
+++ Pro_CT_34.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 
 public class Pro_CT_34 {
-//    자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+    //    자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은
+    //    후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
     public int solution(int n) {
 //        int answer = 0;
 //        String a = Integer.toString(n,3);
@@ -29,25 +30,25 @@ public class Pro_CT_34 {
 }
 
 //########################################
-class Solution {
-    public int solution(int n) {
-        int answer = 0;
-
-        String str = "";
-
-        while(n>0)
-        {
-            int r = n%3;
-            n = n / 3;
-            str = r + str;
-        }
-        System.out.println(str);
-
-        for(int i=0; i<str.length(); i++)
-        {
-            answer += Math.pow(3,i) * (str.charAt(i) - '0');
-        }
-
-        return answer;
-    }
-}
+//class Solution {
+//    public int solution(int n) {
+//        int answer = 0;
+//
+//        String str = "";
+//
+//        while(n>0)
+//        {
+//            int r = n%3;
+//            n = n / 3;
+//            str = r + str;
+//        }
+//        System.out.println(str);
+//
+//        for(int i=0; i<str.length(); i++)
+//        {
+//            answer += Math.pow(3,i) * (str.charAt(i) - '0');
+//        }
+//
+//        return answer;
+//    }
+//}
