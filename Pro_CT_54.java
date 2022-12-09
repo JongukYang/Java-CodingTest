2487d50ead5e04e149f5712a27632e6c69d03a50
JongukYang <didwhddnr123@naver.com>
2022-12-09 20:02:09 +0900

    Update Pro_CT_54.java

--- Pro_CT_54.java
+++ Pro_CT_54.java
@@ -5,31 +5,32 @@ import java.util.HashMap;
 public class Pro_CT_54 {
 //    체육복
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int answer = n - lost.length;
+
         Arrays.sort(lost);
         Arrays.sort(reserve);
-        int cnt= 0;
-        for(int i = 0; i<lost.length; i++) {
-            for(int j = 0; j<reserve.length; j++) {
-                if(lost[i] == reserve[j])
-                {
-                    lost[i] = reserve[i] = -1;
-                    cnt++;
+
+        // 여벌 체육복을 가져온 학생이 도난당한 경우
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
                     break;
                 }
             }
         }
-        for(int i = 0; i<lost.length; i++) {
-            for(int j = 0; j<reserve.length; j++) {
-                if(lost[i] == reserve[j]-1 || lost[i] == reserve[j] +1)
-                {
-                    reserve[i] = -1;
-                    cnt++;
+        // 도난당한 학생에게 체육복 빌려주는 경우
+        for(int i=0; i<lost.length; i++){
+            for(int j=0; j<reserve.length; j++){
+                if((lost[i]-1 == reserve[j]) || (lost[i]+1 == reserve[j])){
+                    answer++;
+                    reserve[j] = -1;
                     break;
                 }
             }
         }
-        answer = n - lost.length + cnt;
         return answer;
     }
 
@@ -41,3 +42,37 @@ public class Pro_CT_54 {
         System.out.println(sol.solution(n, lost, reverse));
     }
 }
+//    public int solution(int n, int[] lost, int[] reserve) {
+//        int answer=n-lost.length;
+//        HashSet<Integer> ko = new HashSet<Integer>();
+//        for(int k : reserve) {
+//            ko.add(k);
+//        }
+//        for(int i =0;i<lost.length;i++) {
+//            if(ko.contains(lost[i])) {
+//                //System.out.println("내껀내가입지");
+//                answer++;
+//                ko.remove(lost[i]);
+//                lost[i]=-1;
+//            }
+//        }
+//
+//
+//        for(int i =0;i<lost.length;i++) {
+//            //System.out.println(i);
+//
+//            if(ko.contains(lost[i]-1)) {
+//                //System.out.println("있다");
+//                answer++;
+//                ko.remove(lost[i]-1);
+//            }else if(ko.contains(lost[i]+1)) {
+//                //System.out.println("있다");
+//                answer++;
+//                ko.remove(lost[i]+1);
+//            }
+//            //System.out.println("없다");
+//        }
+//
+//
+//        return answer;
+//    }
