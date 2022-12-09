50a7f5f5c366b666f6b903b021106ade6ac2f0b3
JongukYang <didwhddnr123@naver.com>
2022-12-10 00:50:55 +0900

    Create Pro_CT_55.java

---
+++ Pro_CT_55.java
@@ -0,0 +1,14 @@
+public class Pro_CT_55 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pro_CT_55 sol = new Pro_CT_55();
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        System.out.println(sol.solution(participant,completion));
+    }
+}
