001fde2230e8cc058e17f2d4946034928e9d4def
JongukYang <didwhddnr123@naver.com>
2022-12-11 20:57:50 +0900

    Create Pro_CT_58.java

---
+++ Pro_CT_58.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Pro_CT_58 {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        String[] arr = s.split("");
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        for(int i =0; i<arr.length; i++) {
+            if(arrayList.contains(arr[i])) {
+                int idx = arrayList.lastIndexOf(arr[i]);
+
+                arrayList.add(arr[i]);
+
+                int lastidx = arrayList.lastIndexOf(arr[i]);
+                answer[i] = lastidx-idx;
+            } else {
+                arrayList.add(arr[i]);
+                answer[i] = -1;
+            }
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pro_CT_58 sol = new Pro_CT_58();
+        String s = "banana";
+        System.out.println(sol.solution(s));
+    }
+}
+
+//class Solution {
+//    public int[] solution(String s) {
+//        int[] answer = new int[s.length()];
+//
+//        HashMap<Character, Integer> map = new HashMap<>();
+//        for(int i=0; i<s.length(); i++) {
+//            if(!map.containsKey(s.charAt(i))) {
+//                answer[i] = -1;
+//                map.put(s.charAt(i), i);
+//            }else {
+//                int before = map.get(s.charAt(i));
+//                answer[i] = i - before;
+//                map.put(s.charAt(i), i);
+//            }
+//        }
+//        return answer;
+//    }
+//}
