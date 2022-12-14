c0f1e6d3aad4f4e1a57dc60632701a08a6440fd0
JongukYang <didwhddnr123@naver.com>
2022-12-14 17:23:46 +0900

    Update Pro_CT_62.java

--- Pro_CT_62.java
+++ Pro_CT_62.java
@@ -76,3 +76,28 @@ public class Pro_CT_62 {
         System.out.println(sol.solution(k, score));
     }
 }
+
+//
+//class Solution {
+//    public int[] solution(int k, int[] score) {
+//        int[] answer = new int[score.length];
+//
+//        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+//
+//        int temp = 0;
+//
+//        for(int i = 0; i < score.length; i++) {
+//
+//            priorityQueue.add(score[i]);
+//            if (priorityQueue.size() > k) {
+//                priorityQueue.poll();
+//            }
+//
+//            answer[i] = priorityQueue.peek();
+//        }
+//
+//
+//
+//        return answer;
+//    }
+//}
