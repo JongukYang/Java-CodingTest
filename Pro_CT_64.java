83aa8d7f4d2b5951056ced24b33b9ed96a2f421d
JongukYang <didwhddnr123@naver.com>
2022-12-16 18:53:24 +0900

    Create Pro_CT_64.java

---
+++ Pro_CT_64.java
@@ -0,0 +1,11 @@
+public class Pro_CT_64 {
+    // 기사단원의 무기
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
