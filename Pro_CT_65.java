4b62f4ca2927eec4759f9088bd3b57ccdba0c8fa
JongukYang <didwhddnr123@naver.com>
2022-12-18 15:44:19 +0900

    Create Pro_CT_65.java

---
+++ Pro_CT_65.java
@@ -0,0 +1,13 @@
+public class Pro_CT_65 {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pro_CT_65 sol = new Pro_CT_65();
+        String[] survey = {"AN", "CF", "MJ", "RT", "NA"};
+        int[] choices = {5, 3, 2, 7, 5};
+        System.out.println(sol.solution(survey, choices));
+    }
+}
