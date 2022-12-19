10483b0f7d38e9fd3a3ead37ec8a0d748841cf8a
JongukYang <didwhddnr123@naver.com>
2022-12-19 21:01:24 +0900

    Create Pro_CT_66.java

---
+++ Pro_CT_66.java
@@ -0,0 +1,14 @@
+public class Pro_CT_66 {
+    // 문자열 나누기
+    public int solution(String s) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Pro_CT_66 sol = new Pro_CT_66();
+        String s = "banana";
+        System.out.println(sol.solution(s));
+    }
+}
