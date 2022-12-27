08780801bec2e1e38cf420ddd4785c4269c9c1a6
JongukYang <didwhddnr123@naver.com>
2022-12-27 18:16:12 +0900

    하샤드수

--- src/Programmers/Prime_Leve1/P_3.java
+++ src/Programmers/Prime_Leve1/P_3.java
@@ -1,4 +1,21 @@
 package Programmers.Prime_Leve1;
+//하샤드 수
+//        문제 설명
+//        양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다. 예를 들어 18의 자릿수 합은 1+8=9이고, 18은 9로 나누어 떨어지므로 18은 하샤드 수입니다. 자연수 x를 입력받아 x가 하샤드 수인지 아닌지 검사하는 함수, solution을 완성해주세요.
+//
 
 public class P_3 {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        P_3 sol = new P_3();
+        int x = 10;
+        System.out.println(x);
+    }
 }
