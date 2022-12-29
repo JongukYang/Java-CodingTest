122345beaef7fe0f124404362174c755e8502b73
JongukYang <didwhddnr123@naver.com>
2022-12-29 15:25:44 +0900

    Update P_8.java

--- src/Programmers/Prime_Leve1/P_8.java
+++ src/Programmers/Prime_Leve1/P_8.java
@@ -1,12 +1,26 @@
 package Programmers.Prime_Leve1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+//문자열 내림차순으로 배치하기
+//문제 설명
+//문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+//s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+//
+//제한 사항
+//str은 길이 1 이상인 문자열입니다.
 public class P_8 {
     public String solution(String s) {
         String answer = "";
-        char arr[] = s.toCharArray();
-        Arrays.sort(arr);
-        StringBuilder sb = new StringBuilder(new String(arr));
-        answer = sb.reverse().toString();
+        char[] charArr = s.toCharArray(); // 배열로 만들어서 정렬하기 위해
+//        System.out.println(charArr);
+        Arrays.sort(charArr);
+
+        StringBuilder sb = new StringBuilder(new String(charArr));
+        sb.reverse();
+        answer = sb.toString();
         return answer;
     }
 
