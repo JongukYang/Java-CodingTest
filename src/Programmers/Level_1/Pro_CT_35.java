989c91d9c57e18de9243b2f288440732036b0c1a
JongukYang <didwhddnr123@naver.com>
2023-01-04 11:09:04 +0900

    Update Pro_CT_35.java

--- src/Programmers/Level_1/Pro_CT_35.java
+++ src/Programmers/Level_1/Pro_CT_35.java
@@ -1,7 +1,7 @@
 package Programmers.Level_1;
 
 import java.util.Arrays;
-
+// 예산
 public class Pro_CT_35 {
     //    부서별로 신청한 금액이 들어있는 배열 d와 예산 budget이 매개변수로 주어질 때, 최대 몇 개의 부서에 물품을 지원할 수 있는지 return
     public int solution(int[] d, int budget) {
