7217777ecf7cd41cceb6b6c62ccdb99fc23ebdea
JongukYang <didwhddnr123@naver.com>
2023-01-09 18:07:22 +0900

    Update BJ_CT_3.java

--- src/Baekjoon/BJ_CT_3.java
+++ src/Baekjoon/BJ_CT_3.java
@@ -16,23 +16,15 @@ public class BJ_CT_3 {
         int N = Integer.parseInt(st.nextToken()); // 4
         int M = Integer.parseInt(st.nextToken()); // 3
 
-        int[][] input = new int[N + 1][N + 1]; // input 배열
         int[][] squared = new int[N + 1][N + 1]; // 좌표를 통해 사각형으로 만들어진 누적 합 배열
-
-        // input 배열 입력
-        for (int i = 1; i <= N; i++) { // N = 4
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                input[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
 
         // 좌표를 통해 사각형으로 만들어진 누적 합 배열
         // squared[i][j] = (1,1)에서 (i,j)까지의 합
         for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
-                // (왼쪽← 값) + (위에↑ 값) - (↖중복되는 대각선 값) + (인풋값)
-                squared[i][j] = squared[i - 1][j] + squared[i][j - 1] - squared[i - 1][j - 1] + input[i][j];
+                // (왼쪽← 값) + (위에↑ 값) - (↖중복되는 대각선 값) + (input 값)
+                squared[i][j] = squared[i - 1][j] + squared[i][j - 1] - squared[i - 1][j - 1] + Integer.parseInt(st.nextToken());
             }
         }
 
