50dc9d1e6f6f1fc4d9ec3b656d5986b28c38245a
JongukYang <didwhddnr123@naver.com>
2023-02-03 23:23:49 +0900

    카펫 - 완전탐색

--- src/Programmers/Level_2/PL2_18.java
+++ src/Programmers/Level_2/PL2_18.java
@@ -3,9 +3,24 @@ package Programmers.Level_2;
 public class PL2_18 {
     // 카펫 - 완전탐색
     public int[] solution(int brown, int yellow) {
-        int[] answer = {};
-        
+        int[] answer = new int[2];
 
+        int tileSum = brown+yellow; // 전체 타일 개수
+        int yellow_sqrt = (int)Math.sqrt(yellow); // yellow의 제곱근 구하기
+
+        for(int i = yellow_sqrt; i>0; i--) {
+            for (int j = yellow_sqrt; j <= brown; j++) {
+                if (i * j == yellow) {
+                    int x = j+2;
+                    int y= i+2;
+                    if ((x * y - yellow) == brown) {
+                        answer[0] = x;
+                        answer[1] = y;
+                        return answer;
+                    }
+                }
+            }
+        }
         return answer;
     }
 
