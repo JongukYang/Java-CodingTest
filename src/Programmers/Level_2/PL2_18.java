52e403495b3b8f37a354b7ad524ec031ae501c8f
JongukYang <didwhddnr123@naver.com>
2023-02-03 21:24:23 +0900

    카펫 - 완전탐색

---
+++ src/Programmers/Level_2/PL2_18.java
@@ -0,0 +1,18 @@
+package Programmers.Level_2;
+
+public class PL2_18 {
+    // 카펫 - 완전탐색
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PL2_18 p = new PL2_18();
+        int brown = 10;
+        int yellow = 2;
+        System.out.println(p.solution(brown, yellow)); // [4,3]
+    }
+}
