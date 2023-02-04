3c7362a49952e508f39318e8fbd19ba2880645e5
JongukYang <didwhddnr123@naver.com>
2023-02-04 21:14:44 +0900

    구명보트 - 그리디 -> 풀이 1

--- src/Programmers/Level_2/PL2_19.java
+++ src/Programmers/Level_2/PL2_19.java
@@ -1,45 +1,40 @@
 package Programmers.Level_2;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class PL2_19 {
     // 구명보트 - 탐욕법(Greedy)
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int mid = limit/2;
-        Arrays.sort(people); // 오름차순
 
-        ArrayList<Integer> low = new ArrayList<>();
-        ArrayList<Integer> high = new ArrayList<>();
-
+        Integer[] integers = new Integer[people.length];
         for(int i = 0; i<people.length; i++) {
-            if (people[i] <= mid) {
-                low.add(people[i]);
-            } else {
-                high.add(people[i]);
-            }
+            integers[i] = people[i];
         }
 
-        int idx = low.size() - 1;
-        for (int i = 0; i < high.size(); i++) {
-            for(int j = idx; j >= 0; j--) {
-                if ((high.get(i) + low.get(j)) <= limit) {
-                    answer++;
-                    idx = j;
-                    break;
-                }
+        // 내림차순 정렬
+        Arrays.sort(integers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
             }
-            if (i == high.size() - 1) {
-                int resLow = low.size() - answer;
-                int resHigh = high.size() - answer;
-                answer += resLow/2 + resLow%2 + resHigh;
-                break;
+        });
+
+
+        int j = integers.length-1;
+        int cnt = 0;
+        for (int i = 0; i <= j; i++) {
+            if (integers[i] + integers[j] <= limit) {
+                j--;
             }
+            cnt++;
         }
 
 
-        return answer;
+        return answer+cnt;
     }
 
     public static void main(String[] args) {
@@ -49,3 +44,35 @@ public class PL2_19 {
         System.out.println(p.solution(people, limit)); // 3
     }
 }
+
+
+//int mid = limit/2;
+//        Arrays.sort(people); // 오름차순
+//
+//        ArrayList<Integer> low = new ArrayList<>();
+//        ArrayList<Integer> high = new ArrayList<>();
+//
+//        for(int i = 0; i<people.length; i++) {
+//            if (people[i] <= mid) {
+//                low.add(people[i]);
+//            } else {
+//                high.add(people[i]);
+//            }
+//        }
+//
+//        int idx = low.size() - 1;
+//        for (int i = 0; i < high.size(); i++) {
+//            for(int j = idx; j >= 0; j--) {
+//                if ((high.get(i) + low.get(j)) <= limit) {
+//                    answer++;
+//                    idx = j;
+//                    break;
+//                }
+//            }
+//            if (i == high.size() - 1) {
+//                int resLow = low.size() - answer;
+//                int resHigh = high.size() - answer;
+//                answer += resLow/2 + resLow%2 + resHigh;
+//                break;
+//            }
+//        }
