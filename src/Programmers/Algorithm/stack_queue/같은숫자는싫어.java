767329c6dba96c625f43661f274372791c529baf
JongukYang <didwhddnr123@naver.com>
2023-05-31 20:45:37 +0900

    Create 같은숫자는싫어.java

---
+++ src/Programmers/Algorithm/stack_queue/같은숫자는싫어.java
@@ -0,0 +1,35 @@
+package Programmers.Algorithm.stack_queue;
+
+import java.util.ArrayDeque;
+import java.util.Stack;
+
+public class 같은숫자는싫어 {
+    static class Solution {
+        public int[] solution(int[] arr) {
+            ArrayDeque<Integer> stack = new ArrayDeque<>();
+            for (int i = 0; i < arr.length; i++) {
+                if (!stack.isEmpty()) {
+                    if (stack.peekLast() == arr[i]) {
+                        continue;
+                    } else {
+                        stack.addLast(arr[i]);
+                    }
+                } else {
+                    stack.addLast(arr[i]);
+                }
+            }
+            int[] answer = new int[stack.size()];
+            for (int i = 0; i < answer.length; i++) {
+                answer[i] = stack.pollFirst();
+            }
+            return answer;
+        }
+
+
+        public static void main(String[] args) {
+            Solution s = new Solution();
+            int[] arr = {1, 1, 3, 3, 0, 1, 1};
+            System.out.println(s.solution(arr));
+        }
+    }
+}
