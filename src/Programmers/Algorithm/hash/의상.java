0b2f30d684d593fb3f8741aecb7e4cb5e354b7c0
JongukYang <didwhddnr123@naver.com>
2023-05-31 16:59:43 +0900

    Create 의상.java

---
+++ src/Programmers/Algorithm/hash/의상.java
@@ -0,0 +1,31 @@
+package Programmers.Algorithm.hash;
+
+import java.util.*;
+
+public class 의상 {
+    static class Solution {
+        public int solution(String[][] clothes) {
+            int answer = 0;
+            HashMap<String, Integer> hashMap = new HashMap<>();
+            for (int i = 0; i < clothes.length; i++) {
+                hashMap.put(clothes[i][1], hashMap.getOrDefault(clothes[i][1], 0) + 1);
+            }
+
+            int count = 1;
+            for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+                count *= entry.getValue() + 1;
+            }
+            answer += count - 1;
+            return answer;
+        }
+
+        public static void main(String[] args) {
+            Solution s = new Solution();
+            String[][] clothes = {
+                    {"yellow_hat", "headgear"},
+                    {"blue_sunglasses", "eyewear"},
+                    {"green_turban", "headgear"}};
+            System.out.println(s.solution(clothes));
+        }
+    }
+}
