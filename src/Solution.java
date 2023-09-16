764e2249966fc6ce4e18a1d1243b6a8d518027d7
JongukYang <didwhddnr123@naver.com>
2023-09-16 14:30:41 +0900

    구름톤

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,17 @@
-public class Solution {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int w = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        int _1rm = (int) Math.floor(w * (1.0 + r / 30.0));
+
+        System.out.println(_1rm);
+    }
 }
